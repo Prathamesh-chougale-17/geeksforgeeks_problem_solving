702af6099e1134960de3795ccf727cd6a31491df
PRATHAMESH CHOUGALE <106848418+Prathamesh-chougale-17@users.noreply.github.com>
2024-04-30 17:26:16 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Add two numbers represented by linked lists/add-two-numbers-represented-by-linked-lists.cpp
@@ -0,0 +1,136 @@
+//{ Driver Code Starts
+// driver
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Linked list Node */
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct Node* buildList(int size)
+{
+    int val;
+    cin>> val;
+    
+    Node* head = new Node(val);
+    Node* tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+
+// } Driver Code Ends
+/* node for linked list:
+
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    public:
+    //Function to add two numbers represented by linked list.
+    struct Node* addTwoLists(struct Node* num1, struct Node* num2)
+    {
+        // code here
+        stack<int> s1,s2;
+    while(num1!=NULL){
+        if(s1.empty() && num1->data>0)
+            s1.push(num1->data);
+        else if(!s1.empty()) s1.push(num1->data);
+        num1=num1->next;
+    }
+    while(num2!=NULL){
+         if(s2.empty() && num2->data>0)
+        s2.push(num2->data);
+        else if(!s2.empty()) s2.push(num2->data);
+        num2=num2->next;
+    }
+    int carry=0;
+    Node* result=NULL;
+    while(s1.empty()==false || s2.empty()==false){
+        int a=0,b=0;
+        if(s1.empty()==false){
+            a=s1.top();s1.pop();
+        }
+        if(s2.empty()==false){
+            b=s2.top();s2.pop();
+        }
+        int total=a+b+carry;
+        Node* temp=new Node(total%10);
+        carry=total/10;
+        if(result==NULL){
+            result=temp;
+        }else{
+            temp->next=result;
+            result=temp;
+        }
+    }
+    if(carry!=0){
+        Node* temp=new Node(carry);
+        temp->next=result;
+        result=temp;
+    }
+    Node* zero=new Node(0);
+    if(result)
+    return result;
+    return zero;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        
+        cin>>n;
+        Node* num1 = buildList(n);
+        
+        cin>>m;
+        Node* num2 = buildList(m);
+        Solution ob;
+        Node* res = ob.addTwoLists(num1,num2);
+        printList(res);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
